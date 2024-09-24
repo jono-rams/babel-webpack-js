b1a3385463cf700f7b9854104e688596e8678da7
c1ph3r-dev <c1ph3r.dev@gmail.com>
2024-09-23 21:19:17 -0400

    webpack and babel

--- webpack.config.js
+++ webpack.config.js
@@ -11,5 +11,17 @@ module.exports = {
     static: {
       directory: path.resolve(__dirname, 'dist')
     }
+  },
+  module: {
+    rules: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env']
+        }
+      }
+    }]
   }
 };
